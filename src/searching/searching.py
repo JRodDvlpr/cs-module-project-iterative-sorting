edd2b08c4cf0de22273b0b4420d3e286e620ba35
JRodDvlpr <Mialienproductions@gmail.com>
2020-10-02 21:30:43 -0400

    :tada: Search done

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,16 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
+    min_value = 0
+    max_value = len(arr) - 1
+    
+    while min_value <= max_value:
+        mid = (max_value + min_value) // 2
+        if target < arr[mid]:
+            max_value = mid - 1
+        elif target > arr[mid]:
+            min_value = mid + 1
+        else:
+            return mid
 
     return -1  # not found
